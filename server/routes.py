765e9f1b1bccbace071e8f6ca14a210edc65fa7c
DaleNaci <dtn22@njit.edu>
2025-10-02 05:06:41 -0400

    Added dynamic file adding

--- server/routes.py
+++ server/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template
+import os
+from flask import render_template, current_app
 
 
 def init_app(app):
     @app.route("/dm")
     def dm_page():
-        sounds = ["brook.wav", "flask.wav"]
+        folder = os.path.join(current_app.root_path, "static", "sounds")
+        sounds = [f for f in os.listdir(folder)]
+
         return render_template("dm.html", sounds=sounds)
     
     @app.route("/player")
